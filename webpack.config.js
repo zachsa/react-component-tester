e335b6cf5f40ac5b8bc04969c9fa6d59f8de3936
Zach Smith <zachsmith.sa@gmail.com>
2019-07-09 15:17:12 +0200

    removed server base config

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,6 @@ module.exports = {
     })
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
     compress: true,
     before: function(app, server) {
       console.log("'before' callback is here")
